3c56fbfef4b34e0d51bc98871e1b6db28b5dfee1
Robert Lord <robert@lord.io>
2015-07-17 23:52:16 -0700

    Simplify parser even more, now down to 13 lines

--- ruby/parser.rb
+++ ruby/parser.rb
@@ -1,17 +1,12 @@
 CODE = '(+ 1 (meow foo bar) (chicken-food blah meow) 3)'
 
 class Rogue
-  def initialize(code)
-    @tokens = code.gsub("(", " ( ").gsub(")", " ) ").split(' ')
-    puts @tokens
-    puts parse.inspect
-  end
-
-  private def parse
+  def self.parse(tokens)
+    return parse(tokens.gsub("(", " ( ").gsub(")", " ) ").split(' ')) if tokens.class == String
     list = []
     loop do
-      case token = @tokens.shift
-        when '(' then list.push parse
+      case token = tokens.shift
+        when '(' then list.push parse(tokens)
         when ')', nil then (return list)
         when /\d+/ then list.push token.to_i
         else list.push token.to_sym
@@ -20,4 +15,4 @@ class Rogue
   end
 end
 
-Rogue.new CODE
+puts Rogue.parse(CODE).inspect
